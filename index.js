f644faafa0299cd2a5849b87d07ca67c89c97c5d
Jules <126172956+Jules-w7@users.noreply.github.com>
2024-02-01 08:50:44 +0100

    Ajout des commentaires
    Ajout des commentaires dans index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Import des modules
 const express = require('express');
 const mqtt = require('mqtt');
 const socketIO = require('socket.io');
 const http = require('http');
 
+// Créer l'application grace à express
 const app = express();
 
+// Définie le dossier ou se trouve tous les documents du site
 app.use(express.static('public'));
 
+// Définie le port auquelle le serveur va s'ouvrir
 const port = 3000;
 
+// Créer la connexion MQTT
 const client = mqtt.connect('mqtt://localhost:1884');
 
+// Function qui va s'éxécuter automatiquement lors du démarrage du serveur
 client.on('connect', () => {
 	console.log("Connection established succesfully");
 	const topic = 'topic';
@@ -27,15 +33,18 @@ client.on('connect', () => {
 	});
 });
 
+// Function qui va s'éxécuter automatiquement lors du démarrage du serveur si il y a une erreur
 client.on('error', (err) => {
         console.error('Error;', err);
         client.end();
 });
 
+// Définie quelle document va être ouvert en premier
 app.get('/', (req, res) => {
         res.redirect('/index.html');
 });
 
+// Appelle quelle port et quelle adresse IP le serveur va s'ouvrir
 app.listen(port, '0.0.0.0', () => {
 	console.log('Server is running on port 3000');
 });
